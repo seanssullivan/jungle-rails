984c7fa910bb86eea888dafd6ffa2c55dff27938
Sean Sullivan <dev@seanmedia.ca>
2020-05-30 00:34:35 +0000

    Add admin categories

--- config/routes.rb
+++ config/routes.rb
@@ -15,6 +15,7 @@ Rails.application.routes.draw do
 
   namespace :admin do
     root to: 'dashboard#show'
+    resources :categories, except: [:edit, :update, :show, :delete]
     resources :products, except: [:edit, :update, :show]
   end
 
