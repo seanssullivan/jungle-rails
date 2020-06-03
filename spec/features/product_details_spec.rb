a4d44757a277edd2c257ac1e74dc43e735e118dd
Sean Sullivan <dev@seanmedia.ca>
2020-06-03 16:25:42 +0000

    Add spec to click from product index to show

---
+++ spec/features/product_details_spec.rb
@@ -0,0 +1,30 @@
+require 'rails_helper'
+
+RSpec.feature "ProductDetails", type: :feature, js: true do
+  
+  before :each do
+    @category = Category.create! name: 'Apparel'
+
+    10.times do |n|
+      @category.products.create!(
+        name: Faker::Hipster.sentence(3),
+        description: Faker::Hipster.paragraph(4),
+        image: open_asset('apparel1.jpg'),
+        quantity: 10,
+        price: 64.99
+      )
+    end
+  end
+
+  scenario "They see all products" do
+    visit root_path
+    within first('.product') do
+      click_on 'Details'
+    end
+
+    save_screenshot
+
+    expect(page).to have_css 'article.product-detail', count: 1
+  end
+
+end
